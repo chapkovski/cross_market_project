fa5b7b6540deb6f7ef67fc697390750936893772
Philipp Chapkovski <chapkovski@gmail.com>
2022-04-12 14:09:50 +0400

    before movig nt quote to start

--- main/tasks.py
+++ main/tasks.py
@@ -9,7 +9,7 @@ bid_types = ['sell', 'buy']
 
 
 @db_task()
-def handle_update(group_id, virtual_id, market, seed_num):
+def handle_update(group_id, virtual_id, market, quote, seed_num):
     virtual = Player.objects.get(id=virtual_id)
     # Check for attainability: inventory condition (cash and shares) and one-side condition (they can post to only
     group = Group.objects.get(id=group_id)
